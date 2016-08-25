4e217c94a501c7fe790833ad8afe47fcc309867f
scheleaap <git@wout.maaskant.info>
2016-08-25 14:06:56 +0200

    fix: man colors

--- modules/man.zsh
+++ modules/man.zsh
@@ -1,11 +1,12 @@
 autoload -U colors && colors
 
 # colorize manpage
+# See also http://unix.stackexchange.com/questions/108699/documentation-on-less-termcap-variables
 function man-colorized {
   env \
     LESS_TERMCAP_md=$(printf "${fg_bold[green]}") \
     LESS_TERMCAP_us=$(printf "${fg_bold[blue]}") \
-    LESS_TERMCAP_ue=$(printf "${fg_bold[default]}") \
+    LESS_TERMCAP_ue=$(printf "${fg_no_bold[default]}") \
     PAGER="${commands[less]:-$PAGER}" \
     _NROFF_U=1 \
     'man' $@
