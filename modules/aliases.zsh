bcb50ba9708f648785397d993051752fa4bd9efd
scheleaap <git@wout.maaskant.info>
2022-09-28 10:42:23 +0200

    fix: Remove aliases for bat

--- modules/aliases.zsh
+++ modules/aliases.zsh
@@ -6,8 +6,6 @@ alias mkdir='mkdir -p'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias cat='batcat'
-#alias less='batcat --paging=always'
 
 # Pretty-print of the PATH variable:
 alias path='echo -e ${PATH//:/\\n}'
